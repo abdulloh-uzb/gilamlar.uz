f8d9e5cad71e2467540ab9f5d6a1153a37b7ac6e
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2025-01-29 15:09:43 +0500

    fake data for style table

--- database/seeders/StyleSeeder.php
+++ database/seeders/StyleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Style;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class StyleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Style::create(["name_uz" => "Klassik dizayn", "name_ru" => "Классические"]);
+        Style::create(["name_uz" => "Neoklassik dizayn", "name_ru" => "Неоклассические"]);
+        Style::create(["name_uz" => "Zamonaviy dizayn", "name_ru" => "Современные"]);
+        Style::create(["name_uz" => "Bolalar dizayni", "name_ru" => "Детские"]);
+
     }
 }
