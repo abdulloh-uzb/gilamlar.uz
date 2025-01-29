72a786c0f2398c5585736f35f21157928b4c7208
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2025-01-29 14:31:57 +0500

    fake data for categories table

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create(["name_uz" => "Gilam", "name_ru" => "Ковры" ]);
+        Category::create(["name_uz" => "Kovrolin", "name_ru" => "Ковролин"]);
     }
 }
